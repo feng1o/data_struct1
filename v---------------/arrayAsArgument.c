9ec0be6f26c3a268757ea85430d4acd34b5a9597
feng1o <liufeng_mail@126.com>
2016-04-12 13:29:47 +0800

    aa

---
+++ v---------------/arrayAsArgument.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "errno.h"
+
+
+
+
+signed char  isStackSeq(int a[][2], int b[][2], int N)
+{
+	return N;
+}
+
+int main()
+{
+#if !defined __cplusplus
+	printf("c++\n");
+#else
+	printf("c.......\n");
+#endif
+	int a[] = { 1, 2, 4, 7, 3, 5 };
+	int b[] = { 2, 1, 3, 4, 7, 5 };
+	isStackSeq(&a, &b, 6);
+	return 0;
+}
