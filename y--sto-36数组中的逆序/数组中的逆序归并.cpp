d568d744effd6a7368c4c8f8aa8e82876298aff4
feng1o <liufeng_mail@126.com>
2016-05-24 12:54:24 +0800

    36

--- y--sto-36数组中的逆序/数组中的逆序归并.cpp
+++ y--sto-36数组中的逆序/数组中的逆序归并.cpp
@@ -17,14 +17,16 @@ public:
 		int lnc = partion(data, begin, mid);
 		int rnc = partion(data, mid + 1, end);
 
-		vector<int> ivec(end - begin + 1, 0);
+		//vector<int> ivec(data);
+		vector<int> ivec(end + 1, 0);
+		//vector<int> ivec(end - begin + 1, 0);///error，后面赋值的时候下标问题，并没有-差
 		int i = mid;
 		int j = end;
 		int index = end;
 		int count = 0;
 		while (i >= begin && j >= mid + 1){
 			if (data[i] > data[j]){
-				count += j - i;
+				count += j - mid;  //这里是--mid不是i，注意，，，
 				ivec[index--] = data[i--];
 			}
 			else{
@@ -42,7 +44,23 @@ public:
 
 	}
 };
-
+/*
+//理解这方法，，，，这个从头开始，比这个数小的应该有机会成为一对，然后后面继续，
+class Solution {
+public:
+	int InversePairs(vector<int> data) {
+		vector<int> a(9, 0);  //用一个辅助数组保存前面有多少个大于 0 - 8的数
+		int sum = 0;
+		for (int i = 0; i < data.size(); i++) {
+			int tmp = data[i];
+			sum += a[tmp];
+			for (int j = tmp - 1; j >= 0; j--)  //更新辅助数组的值
+				a[j] += 1;
+		}
+		return sum;
+	}
+};
+*/
 int main()
 {
 	vector<int> ivec{ 7, 5, 6, 4 };
